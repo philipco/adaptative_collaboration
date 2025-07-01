Related works and fixing weight computation in All-for-one (#12)
Implement Cobo/Ditto/Wga-bc/Apfl algorithms.
Several seeds: 127, 496, 1729 (Mersenne number, Perfect number, Ramanujan number).
Fixthe  error in the weight computation in all-for-one: from norm to squared norm!
Local Algo is independent from all-for-all.
Use a bigger batch size for computing weights.
Inner iteration in ALGO as a global variable.
Change ID for client/pictures/pickle to save settings.
Add cluster and inverse split.
Early stopping for gradient validation evaluation.
Save more hyperparameters in files/pictures/pickles names.
Possibility to do ReducedLROnPlateau.
Possibility to run GD for less than 20clients while still collaborating with everyone.

src/data/DataLoader.py
@@ -18,7 +18,7 @@ from src.data.DataCollatorForMultipleChoice import DataCollatorForMultipleChoice
 from src.data.LiquidAssetDataset import prepare_liquid_asset
 from src.data.DatasetConstants import CHECKPOINT
 from src.data.Split import create_non_iid_split
-from src.data.SyntheticDataset import SyntheticLSRDataset
+from src.data.SyntheticDataset import SyntheticLSRDataset, BinarySynthetic
 from src.utils.Utilities import get_path_to_datasets, print_mem_usage
 
 
@@ -114,12 +114,13 @@ def generate_client_models(N: int, K: int, d: int, cluster_variance: float = 2):
     """
     # Generate K cluster centers
     # Create an uninitialized tensor and fill it with values from the uniform distribution
-    cluster_centers = [torch.empty(d).uniform_(-5, 5) for _ in range(K)]
+    # cluster_centers = [torch.DoubleTensor([10, 10]), -torch.DoubleTensor([5, 5])] #
+    cluster_centers = [torch.empty(d).uniform_(-10, 10) for _ in range(K)]
     # Assign each client to a cluster and generate their model
     variations = [cluster_variance * torch.randn(d) for i in range(N)]
     return [cluster_centers[i % K] + variations[i] for i in range(N)], variations
 
-def get_synth_data(batch_size: int, nb_clients = 4, nb_clusters = 1, dim: int = 2) -> tuple[
+def get_synth_data(batch_size: int, nb_clients = 20, nb_clusters = 1, dim: int = 2, cluster_variance: float = 0.1) -> tuple[
     list[DataLoader[Any]], list[DataLoader[Any]], list[DataLoader[Any]], bool]:
     """Generate synthetic data (LSR) for federated learning experiments.
 
@@ -128,6 +129,7 @@ def get_synth_data(batch_size: int, nb_clients = 4, nb_clusters = 1, dim: int = 
         nb_clients (int, optional): Number of clients. Defaults to 4.
         nb_clusters (int, optional): Number of clusters. Defaults to 1.
         dim (int, optional): Dimensionality of the data. Defaults to 2.
+        cluster_variance (float, optional): Variance of optimal models inside a cluster. Defaults to 0.01/
 
     Returns:
         Tuple[List[DataLoader], List[DataLoader], List[DataLoader], bool]:
@@ -136,7 +138,7 @@ def get_synth_data(batch_size: int, nb_clients = 4, nb_clusters = 1, dim: int = 
             - test_loaders: List of DataLoaders for test data.
             - natural_split: Boolean indicating if the data split is natural.
         """
-    true_models, variations = generate_client_models(nb_clients, nb_clusters, dim, cluster_variance=0.1)
+    true_models, variations = generate_client_models(nb_clients, nb_clusters, dim, cluster_variance=cluster_variance)
     datasets = [SyntheticLSRDataset(m, v, batch_size) for (m, v) in zip(true_models, variations)]
 
     train_loaders = [DataLoader(d, batch_size=None) for d in datasets]
@@ -147,7 +149,7 @@ def get_synth_data(batch_size: int, nb_clients = 4, nb_clusters = 1, dim: int = 
     return train_loaders, val_loaders, test_loaders, natural_split
 
 
-def get_data_from_pytorch(dataset_name: str, fed_dataset, nb_of_clients, split_type, kwargs_train_dataset, kwargs_test_dataset,
+def get_data_from_pytorch(dataset_name: str, fed_dataset, nb_of_clients, split_type, batch_size_alignement, kwargs_train_dataset, kwargs_test_dataset,
                           kwargs_dataloader) -> tuple[list[DataLoader[Any]], list[DataLoader[Any]], list[DataLoader[Any]], bool]:
     """Load and preprocess data from PyTorch datasets for federated learning.
 
@@ -190,12 +192,12 @@ def get_data_from_pytorch(dataset_name: str, fed_dataset, nb_of_clients, split_t
     X_train, X_val, X_test, Y_train, Y_val, Y_test = [], [], [], [], [], []
     for (x,y) in zip(X, Y):
         x2, x_test, y2, y_test = train_test_split(x, y, test_size=0.2, random_state=2024)
-        x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.1, random_state=2024)
-        X_train.append(x_train)
-        X_val.append(x_val)
+        # x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.1, random_state=2024)
+        X_train.append(x2)
+        # X_val.append(x_val)
         X_test.append(x_test)
-        Y_train.append(y_train)
-        Y_val.append(y_val)
+        Y_train.append(y2)
+        # Y_val.append(y_val)
         Y_test.append(y_test)
 
     print_mem_usage()
@@ -207,15 +209,24 @@ def get_data_from_pytorch(dataset_name: str, fed_dataset, nb_of_clients, split_t
 
     print_mem_usage()
 
-    train_loaders = [DataLoader(TensorDataset(X_train[i], Y_train[i]), **kwargs_dataloader) for i in range(nb_of_clients)]
-    val_loaders = [DataLoader(TensorDataset(X_val[i], Y_val[i]), **kwargs_dataloader) for i in range(nb_of_clients)]
-    test_loaders = [DataLoader(TensorDataset(X_test[i], Y_test[i]), **kwargs_dataloader) for i in range(nb_of_clients)]
+    if split_type == "inverse":
+        train_loaders = [DataLoader(BinarySynthetic(X_train[i], Y_train[i], i), **kwargs_dataloader) for i in
+                         range(nb_of_clients)]
+        test_loaders = [DataLoader(BinarySynthetic(X_train[i], Y_train[i], i), **kwargs_dataloader) for i in
+                        range(nb_of_clients)]
+        kwargs_dataloader["batch_size"] = batch_size_alignement
+        val_loaders = [DataLoader(BinarySynthetic(X_train[i], Y_train[i], i), **kwargs_dataloader) for i in
+                       range(nb_of_clients)]
+    else:
+        train_loaders = [DataLoader(TensorDataset(X_train[i], Y_train[i]), **kwargs_dataloader) for i in range(nb_of_clients)]
+        test_loaders = [DataLoader(TensorDataset(X_test[i], Y_test[i]), **kwargs_dataloader) for i in range(nb_of_clients)]
+        kwargs_dataloader["batch_size"] = batch_size_alignement
+        val_loaders = [DataLoader(TensorDataset(X_train[i], Y_train[i]), **kwargs_dataloader) for i in range(nb_of_clients)]
 
     natural_split = False
     return train_loaders, val_loaders, test_loaders, natural_split
 
-
-def get_data_from_flamby(fed_dataset, nb_of_clients, dataset_name: str, batch_size, kwargs_dataloader, debug: bool = False) \
+def get_data_from_flamby(dataset_name: str, fed_dataset, nb_of_clients, batch_size_alignement: int, kwargs_dataloader, debug: bool = False) \
         -> tuple[list[DataLoader[Any]], list[DataLoader[Any]], list[DataLoader[Any]], bool]:
     """Load and preprocess data from FLamby datasets for federated learning.
 
@@ -259,11 +270,11 @@ def get_data_from_flamby(fed_dataset, nb_of_clients, dataset_name: str, batch_si
         X_test.append(torch.concat([data_test]))
         Y_test.append(torch.concat([labels_test]))
 
-    train_loaders = [DataLoader(TensorDataset(X_train[i], Y_train[i]), batch_size=batch_size) for i in
+    train_loaders = [DataLoader(TensorDataset(X_train[i], Y_train[i]), **kwargs_dataloader) for i in
                      range(nb_of_clients)]
-    val_loaders = [DataLoader(TensorDataset(X_val[i], Y_val[i]), batch_size=batch_size) for i in range(nb_of_clients)]
-    test_loaders = [DataLoader(TensorDataset(X_test[i], Y_test[i]), batch_size=batch_size) for i in
-                    range(nb_of_clients)]
+    test_loaders = [DataLoader(TensorDataset(X_test[i], Y_test[i]), **kwargs_dataloader) for i in range(nb_of_clients)]
+    kwargs_dataloader["batch_size"] = batch_size_alignement
+    val_loaders = [DataLoader(TensorDataset(X_train[i], Y_train[i]), **kwargs_dataloader) for i in range(nb_of_clients)]
 
     natural_split = True
     return train_loaders, val_loaders, test_loaders, natural_split

src/data/DatasetConstants.py
@@ -1,6 +1,7 @@
 import torchvision
 import torch.nn as nn
 from torch.nn import MSELoss
+from torchvision import models
 from torchvision.transforms import transforms
 
 from src.optim.CustomLoss import DiceLoss, CoxLoss, L1WeightedAccuracyLoss
@@ -8,8 +9,6 @@ from src.optim.Metric import dice, auc, c_index, accuracy, l1_accuracy, mse_accu
 from src.optim.nn.Nets import TcgaRegression, CNN_MNIST, HeartDiseaseRegression, LiquidAssetRegression, \
     Synth2ClientsRegression, Synth100ClientsRegression, LeNet
 from src.optim.nn.Unet import UNet
-
-PCA_NB_COMPONENTS = 16
 
 TRANSFORM_MNIST = torchvision.transforms.Compose([
         torchvision.transforms.ToTensor(),
@@ -30,18 +29,41 @@ TRANSFORM_TEST_CIFAR10 = transforms.Compose([
                          std=(0.2023, 0.1994, 0.2010))
 ])
 
-MODELS = {"mnist": CNN_MNIST, "mnist_iid": CNN_MNIST, "cifar10": LeNet, "cifar10_iid": LeNet,
+MODELS = {"mnist": CNN_MNIST,
+          "mnist_iid": CNN_MNIST,
+          "cifar10": LeNet, #models.googlenet,
+          "cifar10_iid": LeNet,
           "heart_disease": HeartDiseaseRegression,
-          "tcga_brca": TcgaRegression, "ixi": UNet, "liquid_asset": LiquidAssetRegression, "synth": Synth2ClientsRegression,
+          "tcga_brca": TcgaRegression,
+          "ixi": UNet,
+          "liquid_asset": LiquidAssetRegression,
+          "synth": Synth2ClientsRegression,
+          "synth_iid": Synth2ClientsRegression,
           "synth_complex":  Synth100ClientsRegression,
           "exam_llm": None}
-CRITERION = {"mnist": nn.CrossEntropyLoss, "mnist_iid": nn.CrossEntropyLoss, "cifar10": nn.CrossEntropyLoss, "cifar10_iid": nn.CrossEntropyLoss,
+CRITERION = {"mnist": nn.CrossEntropyLoss,
+             "mnist_iid": nn.CrossEntropyLoss,
+             "cifar10": nn.CrossEntropyLoss,
+             "cifar10_iid": nn.CrossEntropyLoss,
              "heart_disease": nn.BCELoss,
-             "tcga_brca": CoxLoss, "ixi": DiceLoss, "liquid_asset": L1WeightedAccuracyLoss, "synth": MSELoss,
+             "tcga_brca": CoxLoss,
+             "ixi": DiceLoss,
+             "liquid_asset": L1WeightedAccuracyLoss,
+             "synth": MSELoss,
+             "synth_iid": MSELoss,
              "synth_complex": MSELoss,
              "exam_llm": nn.CrossEntropyLoss}
-METRIC =  {"mnist": accuracy, "mnist_iid": accuracy, "cifar10": accuracy, "cifar10_iid": accuracy, "heart_disease": auc, "tcga_brca": c_index, "ixi": dice,
-           "liquid_asset": l1_accuracy, "synth": mse_accuracy, "synth_complex": mse_accuracy,
+METRIC =  {"mnist": accuracy,
+           "mnist_iid": accuracy,
+           "cifar10": accuracy,
+           "cifar10_iid": accuracy,
+           "heart_disease": auc,
+           "tcga_brca": c_index,
+           "ixi": dice,
+           "liquid_asset": l1_accuracy,
+           "synth": mse_accuracy,
+           "synth_iid": mse_accuracy,
+           "synth_complex": mse_accuracy,
            "exam_llm": accuracy}
 
 CHECKPOINT = "distilbert-base-uncased"
@@ -51,28 +73,96 @@ TRANSFORM_TRAIN = {"mnist": TRANSFORM_MNIST, "mnist_iid": TRANSFORM_MNIST, "cifa
 TRANSFORM_TEST= {"mnist": TRANSFORM_MNIST, "mnist_iid": TRANSFORM_MNIST, "cifar10": TRANSFORM_TEST_CIFAR10, "cifar10_iid": TRANSFORM_TEST_CIFAR10,
                  "liquid_asset": None}
 
-NB_CLIENTS = {"mnist": 20, "mnist_iid": 20, "fashion_mnist": 8, "cifar10": 20, "cifar10_iid": 20, "camelyon16": 2, "heart_disease": 4, "isic2019": 6,
-              "ixi": 3, "kits19": 6, "lidc_idri": 5, "tcga_brca": 6, "liquid_asset": 100, "synth": 20,
+NB_CLIENTS = {"mnist": 20,
+              "mnist_iid": 20,
+              "fashion_mnist": 8,
+              "cifar10": 20,
+              "cifar10_iid": 20,
+              "camelyon16": 2,
+              "heart_disease": 4,
+              "isic2019": 6,
+              "ixi": 3,
+              "kits19": 6,
+              "lidc_idri": 5,
+              "tcga_brca": 6,
+              "liquid_asset": 100,
+              "synth": 20,
+              "synth_iid": 20,
               "synth_complex": 20,
               "exam_llm": 3}
-STEP_SIZE = {"mnist": 0.1, "mnist_iid": 0.1, "cifar10": 0.1, "cifar10_iid": 0.1, "tcga_brca": .015, "heart_disease": 0.05,
-             "ixi": 0.01, "liquid_asset": 0.01, "synth": None, "synth_complex": None,
+STEP_SIZE = {"mnist": 0.1,
+             "mnist_iid": 0.1,
+             "cifar10": 0.001,
+             "cifar10_iid": 0.1,
+             "tcga_brca": .015,
+             "heart_disease": 0.01,
+             "ixi": 0.01,
+             "liquid_asset": 0.01,
+             "synth": None,
+             "synth_iid": None,
+             "synth_complex": None,
              "exam_llm": 0.001}
-WEIGHT_DECAY = {"mnist": 5*10**-4, "mnist_iid": 5*10**-4, "cifar10": 5*10**-4, "cifar10_iid": 5*10**-4, "tcga_brca": 0, "heart_disease": 0,
-                "ixi": 5*10**-4, "liquid_asset": 5*10**-4, "synth": 0, "synth_complex": 0, "exam_llm": 5*10**-4}
-BATCH_SIZE = {"mnist": 64, "mnist_iid": 64, "cifar10": 64, "cifar10_iid": 64, "tcga_brca": 8, "heart_disease": 1,
-              "ixi": 8, "liquid_asset": 8, "synth": 1, "synth_complex": 1, "exam_llm": 8}
-MOMENTUM = {"mnist": 0., "mnist_iid": 0., "cifar10": 0.9, "cifar10_iid": 0.9, "tcga_brca": 0, "heart_disease": 0,
-            "ixi": 0.9, "liquid_asset": 0.9, "synth": 0, "synth_complex":0, "exam_llm": 0.9}
-SCHEDULER_PARAMS = {"mnist": (10, 0.1), "mnist_iid": (10, 0.1), "cifar10": (10, 0.1), "cifar10_iid": (10, 0.1), "tcga_brca": (50, 0.609),
+WEIGHT_DECAY = {"mnist": 5*10**-4,
+                "mnist_iid": 5*10**-4,
+                "cifar10": 5*10**-4,
+                "cifar10_iid": 5*10**-4,
+                "tcga_brca": 0,
+                "heart_disease": 0,
+                "ixi": 5*10**-4,
+                "liquid_asset": 5*10**-4,
+                "synth": 0,
+                "synth_iid": 0,
+                "synth_complex": 0,
+                "exam_llm": 5*10**-4}
+BATCH_SIZE = {"mnist": 16,
+              "mnist_iid": 64,
+              "cifar10": 16,
+              "cifar10_iid": 64,
+              "tcga_brca": 8,
+              "heart_disease": 1,
+              "ixi": 8,
+              "liquid_asset": 8,
+              "synth": 2,
+              "synth_iid": 2,
+              "synth_complex": 2,
+              "exam_llm": 8}
+MOMENTUM = {"mnist": 0.,
+            "mnist_iid": 0.,
+            "cifar10": 0.9,
+            "cifar10_iid": 0.9,
+            "tcga_brca": 0,
+            "heart_disease": 0,
+            "ixi": 0.9,
+            "liquid_asset": 0.9,
+            "synth": 0,
+            "synth_iid": 0,
+            "synth_complex":0,
+            "exam_llm": 0.9}
+SCHEDULER_PARAMS = {"mnist": (200, 0.1),
+                    "mnist_iid": (10, 0.1),
+                    "cifar10": (100, 0.1),
+                    "cifar10_iid": (10, 0.1),
+                    "tcga_brca": (50, 0.609),
                     "heart_disease": (5, 0.1),
-                    "ixi": (10, 0.1), "liquid_asset": (1, 0.9), "synth": (1, 1), "synth_complex": (1, 1),
+                    "ixi": (10, 0.1),
+                    "liquid_asset": (1, 0.9),
+                    "synth": (1, 1),
+                    "synth_iid": (1, 1),
+                    "synth_complex": (1, 1),
                     "exam_llm": (10, 0.1)}
-NB_EPOCHS = {"mnist": 50, "mnist_iid": 50, "cifar10": 50, "cifar10_iid": 50, "tcga_brca": 50, "heart_disease": 25,
-            "ixi": 10, "liquid_asset": 50, "synth": 100, "synth_complex": 200, "exam_llm": 4}
+NB_EPOCHS = {"mnist": 20,
+             "mnist_iid": 50,
+             "cifar10": 100,
+             "cifar10_iid": 50,
+             "tcga_brca": 50,
+             "heart_disease": 20,
+             "ixi": 10,
+             "liquid_asset": 50,
+             "synth": 40,
+             "synth_iid": 40,
+             "synth_complex": 200,
+             "exam_llm": 4}
 
+SPLIT = {"mnist": "cluster", "cifar10": "cluster"}
 
-
-
-
-
+RUNNING_CLIENTS = -1

src/data/Split.py
@@ -55,6 +55,11 @@ def iid_split(data: np.ndarray, labels: np.ndarray, nb_clients: int) -> [List[np
         Y.append(labels[split_indices[i]])
     return X, Y
 
+    # for i in range(nb_clients):
+    #     X.append(data[indices][:400])
+    #     Y.append(labels[indices][:400])
+    # return X, Y
+
 
 def create_non_iid_split(features: List[np.ndarray], labels: List[np.ndarray], nb_clients: int,
                          split_type: str, dataset_name: str) -> [List[np.ndarray], List[np.ndarray]]:
@@ -73,18 +78,22 @@ def create_non_iid_split(features: List[np.ndarray], labels: List[np.ndarray], n
     """
     np.random.seed(2024)
 
-    if split_type == "iid":
+    if split_type in ["iid", "inverse"]:
         print("IID split.")
         return iid_split(features, labels, nb_clients)
 
-    if split_type == "dirichlet":
+    elif split_type == "dirichlet":
         print("Dirichlet split")
         alpha = 0.1 if dataset_name == "mnist" else 1
         return dirichlet_split(features, labels, nb_clients, alpha)
 
-    if split_type == "partition":
+    elif split_type == "partition":
         print("Partition split.")
         return sort_and_partition_split(features, labels, nb_clients)
+
+    elif split_type == "cluster":
+        print("Cluster split.")
+        return cluster_split(features, labels, nb_clients)
 
 
 def sort_and_partition_split(features: np.ndarray, labels: np.ndarray, nb_clients: int) \
@@ -135,6 +144,79 @@ def sort_and_partition_split(features: np.ndarray, labels: np.ndarray, nb_client
         Y[idx_client] = torch.concat(Y[idx_client])
 
     return X, Y
+
+
+def cluster_split(features: np.ndarray, labels: np.ndarray, nb_clients: int) -> (
+        [List[np.ndarray], List[np.ndarray]]):
+    client_features = []
+    client_labels = []
+
+    mask = np.isin(labels, [0, 1, 2, 3, 4])
+
+
+    for i in range(nb_clients):
+        if i % 2 == 1:
+            # Odd clients: labels 1 to 4
+            nb_points_by_clients = int(2 * len(labels[mask]) / nb_clients)
+            client_features.append(features[mask][nb_points_by_clients * (i // 2): nb_points_by_clients * (i // 2 + 1)])
+            client_labels.append(labels[mask][nb_points_by_clients * (i // 2): nb_points_by_clients * (i // 2 +1)])
+
+        else:
+            # Even clients: labels 5 to 9
+            nb_points_by_clients = int(2 * len(labels[~mask]) / nb_clients)
+            client_features.append(features[~mask][nb_points_by_clients * (i // 2): nb_points_by_clients * (i // 2 + 1)])
+            client_labels.append(labels[~mask][nb_points_by_clients * (i // 2): nb_points_by_clients * (i // 2 + 1)])
+
+    return client_features, client_labels
+
+
+def dirichlet_split(data: np.ndarray, labels: np.ndarray, nb_clients: int, dirichlet_coef: float = 1.0) \
+        -> [List[np.ndarray], List[np.ndarray]]:
+    """
+    Non-IID split using Dirichlet distribution over labels.
+
+    Simulates client-specific data preferences where label distributions are sampled
+    from a Dirichlet distribution.
+
+    Args:
+        data (np.ndarray): Feature matrix.
+        labels (np.ndarray): Corresponding labels.
+        nb_clients (int): Number of clients.
+        dirichlet_coef (float): Dirichlet distribution coefficient (smaller => more heterogeneous).
+
+    Returns:
+        Tuple[List[np.ndarray], List[np.ndarray]]: Features and labels for each client.
+    """
+    nb_labels = len(np.unique(labels))  # Total number of unique labels
+
+    X, Y = [[] for _ in range(nb_clients)], [[] for _ in range(nb_clients)]
+
+    for idx_label in range(nb_labels):
+        # Sample label proportions for each client
+        proportions = np.random.dirichlet(np.repeat(dirichlet_coef, nb_clients))
+        assert round(proportions.sum()) == 1, "The sum of proportions is not equal to 1."
+
+        # Get all samples for the current label
+        label_data = data[labels == idx_label]
+        label_targets = labels[labels == idx_label]
+        N = len(label_targets)
+
+        # Compute split indices and assign to clients
+        split_indices = [np.sum([int(proportions[k] * N) for k in range(j)]) for j in range(1, nb_clients)]
+        features_split = np.split(label_data, split_indices)
+        labels_split = np.split(label_targets, split_indices)
+
+        for j in range(nb_clients):
+            X[j].append(features_split[j])
+            Y[j].append(labels_split[j])
+
+    # Final concatenation for each client
+    for idx_client in range(nb_clients):
+        X[idx_client] = torch.concat(X[idx_client])
+        Y[idx_client] = torch.concat(Y[idx_client])
+
+    return X, Y
+
 
 
 def dirichlet_split(data: np.ndarray, labels: np.ndarray, nb_clients: int, dirichlet_coef: float = 1.0) \

src/data/SyntheticDataset.py
@@ -13,7 +13,7 @@ loss are estimated using the empirical covariance of the features.
 import torch
 from numpy.random import multivariate_normal
 from scipy.stats import ortho_group
-from torch.utils.data import IterableDataset
+from torch.utils.data import IterableDataset, Dataset
 
 
 class SyntheticLSRDataset(IterableDataset):
@@ -123,3 +123,26 @@ class SyntheticLSRDataset(IterableDataset):
         print("Strong convexity constant:", mu)
 
         return lips, mu
+
+
+class BinarySynthetic(Dataset):
+    def __init__(self, data, targets, client_id):
+        self.data = data
+        self.targets = targets.clone()
+        even_digits = torch.tensor([0, 1, 2, 6, 8])
+        is_even = client_id % 2 == 0
+
+        # Map labels based on client index parity
+        even_mask = torch.isin(self.targets, even_digits)
+        if is_even:
+            self.targets[even_mask] = 0
+            self.targets[~even_mask] = 1
+        else:
+            self.targets[even_mask] = 1
+            self.targets[~even_mask] = 0
+
+    def __len__(self):
+        return len(self.targets)
+
+    def __getitem__(self, idx):
+        return self.data[idx], self.targets[idx]

src/run_experiments.py
@@ -7,13 +7,12 @@ import argparse
 import torch
 
 from src.data.LiquidAssetDataset import do_prediction_liquid_asset, load_liquid_dataset_test
-from src.data.DatasetConstants import NB_EPOCHS
+from src.data.DatasetConstants import RUNNING_CLIENTS, NB_EPOCHS
 from src.data.Network import get_network
-from src.optim.Algo import fedavg_training, all_for_all_algo, all_for_one_algo, fednova_training
+from src.optim.Algo import fedavg_training, all_for_all_algo, all_for_one_algo, fednova_training, cobo_algo, ditto_algo, \
+    wga_bc_algo, apfl_algo, local_training
 from src.utils.PlotUtilities import plot_values, plot_weights
 from src.utils.Utilities import get_path_to_datasets
-
-NB_RUN = 1
 
 if __name__ == '__main__':
 
@@ -27,12 +26,28 @@ if __name__ == '__main__':
         help="Name of the dataset.",
         required=True,
     )
+    parser.add_argument(
+        "--inner_iterations",
+        type=int,
+        help="Number of inner iterations (if not provided, defaults is None leading to take the dataset's size).",
+        required=False,
+        default=50,
+    )
+    parser.add_argument(
+        "--batch_size_alignement",
+        type=int,
+        help="Batch size for gradient alignement is weights computations.",
+        required=False,
+        default=512
+    )
     args = parser.parse_args()
     dataset_name = args.dataset_name
+    inner_iterations = args.inner_iterations
+    batch_size_alignement = args.batch_size_alignement
 
     assert dataset_name in ["exam_llm", "mnist", "mnist_iid", "cifar10", "cifar10_iid", "heart_disease", "tcga_brca", "ixi", "liquid_asset",
                             "synth", "synth_complex"], "Dataset not recognized."
-    print(f"### ================== DATASET: {dataset_name} ================== ###")
+    print(f"### ================== DATASET: {dataset_name} - inner iterations: {inner_iterations} ================== ###")
 
     nb_epochs = NB_EPOCHS[dataset_name]
 
@@ -40,55 +55,74 @@ if __name__ == '__main__':
     if "synth" in dataset_name:
         torch.set_default_dtype(torch.float64)
 
-    all_algos = ["All-for-one-bin", "All-for-one-cont", "Local", "FedAvg"]
+    all_algos = ["All-for-one-bin", "All-for-one-cont", "Local", "FedAvg", "Ditto", "Cobo", "Wga-bc", "Apfl"]
+    all_seeds = [127, 496, 1729] # Mersenne number, Perfect number, Ramanujan number
 
-    train_epochs, train_losses, train_accuracies = {algo: [] for algo in all_algos}, {algo: [] for algo in all_algos}, {
-        algo: [] for algo in all_algos}
-    test_epochs, test_losses, test_accuracies = {algo: [] for algo in all_algos}, {algo: [] for algo in all_algos}, {
-        algo: [] for algo in all_algos}
-    weights, ratio = {algo: [] for algo in all_algos}, {algo: [] for algo in all_algos}
+    def dict(all_algos, all_seeds):
+        return {algo: {s: [] for s in all_seeds} for algo in all_algos}
+
+    train_epochs, train_losses, train_accuracies = dict(all_algos, all_seeds), dict(all_algos, all_seeds), dict(all_algos, all_seeds)
+    test_epochs, test_losses, test_accuracies = dict(all_algos, all_seeds), dict(all_algos, all_seeds), dict(all_algos, all_seeds)
+    weights, ratio = dict(all_algos, all_seeds), dict(all_algos, all_seeds)
     
     for algo_name in all_algos:
-        assert algo_name in ["All-for-one-bin", "All-for-one-cont", "All-for-all", "Local", "FedAvg", "FedNova"], "Algorithm not recognized."
+
+        assert algo_name in ["All-for-one-bin", "All-for-one-cont", "All-for-all", "Local", "FedAvg", "FedNova",
+                             "Cobo", "Ditto", "Wga-bc", "Apfl"], "Algorithm not recognized."
         print(f"--- ================== ALGO: {algo_name} ================== ---")
 
-        network = get_network(dataset_name, algo_name)
+        for seed in all_seeds:
+            network = get_network(dataset_name, algo_name, initial_seed=seed, inner_iterations=inner_iterations,
+                                  batch_size_alignement=batch_size_alignement)
 
-        if algo_name == "FedAvg":
-            fedavg_training(network, nb_of_synchronization=nb_epochs)
-        elif algo_name == "All-for-all":
-            all_for_all_algo(network, nb_of_synchronization=nb_epochs, collab_based_on="ratio")
-        elif algo_name == "Local":
-            all_for_all_algo(network, nb_of_synchronization=nb_epochs, collab_based_on="local")
-        elif algo_name == "Fednova":
-            fednova_training(network, nb_of_synchronization=nb_epochs)
-        elif algo_name == "All-for-one-bin":
-            all_for_one_algo(network, nb_of_synchronization=nb_epochs, continuous=False)
-        elif algo_name == "All-for-one-cont":
-            all_for_one_algo(network, nb_of_synchronization=nb_epochs, continuous=True)
+            if algo_name == "FedAvg":
+                fedavg_training(network, nb_of_synchronization=nb_epochs)
+            elif algo_name == "All-for-all":
+                all_for_all_algo(network, nb_of_synchronization=nb_epochs, collab_based_on="ratio")
+            elif algo_name == "Local":
+                local_training(network, nb_of_synchronization=nb_epochs)
+            elif algo_name == "FedNova":
+                fednova_training(network, nb_of_synchronization=nb_epochs)
+            elif algo_name == "All-for-one-bin":
+                all_for_one_algo(network, nb_of_synchronization=nb_epochs, continuous=False)
+            elif algo_name == "All-for-one-cont":
+                all_for_one_algo(network, nb_of_synchronization=nb_epochs, continuous=True)
+            elif algo_name == "Cobo":
+                cobo_algo(network, nb_of_synchronization=nb_epochs)
+            elif algo_name == "Ditto":
+                ditto_algo(network, nb_of_synchronization=nb_epochs)
+            elif algo_name == "Wga-bc":
+                wga_bc_algo(network, nb_of_synchronization=nb_epochs)
+            elif algo_name == "Apfl":
+                apfl_algo(network, nb_of_synchronization=nb_epochs)
 
-        for client in network.clients:
-            writer = client.writer
+            for client in network.clients[:RUNNING_CLIENTS]:
+                writer = client.writer
 
-            train_epochs[algo_name].append(writer.retrieve_information("train_accuracy")[0])
-            train_accuracies[algo_name].append(writer.retrieve_information("train_accuracy")[1])
-            train_losses[algo_name].append(writer.retrieve_information("train_loss")[1])
+                train_epochs[algo_name][seed].append(writer.retrieve_information("train_accuracy")[0])
+                train_accuracies[algo_name][seed].append(writer.retrieve_information("train_accuracy")[1])
+                train_losses[algo_name][seed].append(writer.retrieve_information("train_loss")[1])
 
-            test_epochs[algo_name].append(writer.retrieve_information("test_accuracy")[0])
-            test_accuracies[algo_name].append(writer.retrieve_information("test_accuracy")[1])
-            test_losses[algo_name].append(writer.retrieve_information("test_loss")[1])
+                test_epochs[algo_name][seed].append(writer.retrieve_information("test_accuracy")[0])
+                test_accuracies[algo_name][seed].append(writer.retrieve_information("test_accuracy")[1])
+                test_losses[algo_name][seed].append(writer.retrieve_information("test_loss")[1])
 
-            weights[algo_name].append(writer.retrieve_histogram_information("weights")[1])
-            ratio[algo_name].append(writer.retrieve_histogram_information("ratio")[1])
+                weights[algo_name][seed].append(writer.retrieve_histogram_information("weights")[1])
+                ratio[algo_name][seed].append(writer.retrieve_histogram_information("ratio")[1])
 
-    plot_values(train_epochs, train_accuracies, all_algos, 'Train accuracy', dataset_name)
-    plot_values(train_epochs, train_losses, all_algos, 'log(Train loss)', dataset_name, log=True)
-    plot_values(test_epochs, test_accuracies, all_algos, 'Test accuracy', dataset_name)
-    plot_values(test_epochs, test_losses, all_algos, 'log(Test loss)', dataset_name, log=True)
+    plot_values(train_epochs, train_accuracies, all_algos, 'Train accuracy', dataset_name, inner_iterations,
+                batch_size_alignement)
+    plot_values(train_epochs, train_losses, all_algos, 'log(Train loss)', dataset_name, inner_iterations,
+                batch_size_alignement, log=True)
+    plot_values(test_epochs, test_accuracies, all_algos, 'Test accuracy', dataset_name, inner_iterations,
+                batch_size_alignement)
+    plot_values(test_epochs, test_losses, all_algos, 'log(Test loss)', dataset_name, inner_iterations,
+                batch_size_alignement, log=True)
 
     for algo_name in all_algos:
-        if algo_name in ["All-for-one-bin", "All-for-one-cont", "All-for-all"]:
-            plot_weights(weights[algo_name], dataset_name, algo_name)#, x_axis=test_accuracies[algo_name])
+        if algo_name in ["All-for-one-bin", "All-for-one-cont", "All-for-all", "Cobo"]:
+            plot_weights(weights[algo_name][all_seeds[0]], dataset_name, algo_name, inner_iterations,
+                batch_size_alignement)#, x_axis=test_accuracies[algo_name])
 
     if dataset_name in ["liquid_asset"]:
         X_raw_train, X_raw_test, numerical_transformer = load_liquid_dataset_test(get_path_to_datasets())

src/utils/PlotUtilities.py
@@ -1,6 +1,7 @@
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy import linspace
 
 # Configure matplotlib to use LaTeX for all text rendering
 matplotlib.rcParams.update({
@@ -11,15 +12,17 @@ matplotlib.rcParams.update({
     'text.latex.preamble': r'\usepackage{amsfonts}'
 })
 
-from src.data.DatasetConstants import BATCH_SIZE, STEP_SIZE, MOMENTUM
+from src.data.DatasetConstants import BATCH_SIZE, STEP_SIZE, MOMENTUM, NB_CLIENTS, SPLIT, SCHEDULER_PARAMS
 from src.utils.Utilities import get_project_root, create_folder_if_not_existing
 
 # Default plotting styles
-COLORS = ['tab:blue', 'tab:red', 'tab:orange', 'tab:brown', 'tab:green', 'tab:purple']
-MARKERS = ['o', 's', 'D', '^', 'v', '<']
-FONTSIZE = 25
+COLORS = ['tab:blue', 'tab:red', 'tab:orange', 'tab:brown', 'tab:green', 'tab:purple', 'tab:cyan', 'tab:pink',
+          'tab:grey']
+MARKERS = ['o', 's', 'D', '^', 'v', '<', 'P', 'X']
+FONTSIZE = 20
 
-def plot_values(epochs, values, legends, metric_name, dataset_name, log=False):
+def plot_values(epochs, values, legends, metric_name: str, dataset_name: str, inner_iterations: int, batch_size_alignement: int,
+                log=False):
     """
     Plot the mean and standard deviation of a metric over epochs for multiple algorithms.
 
@@ -31,20 +34,27 @@ def plot_values(epochs, values, legends, metric_name, dataset_name, log=False):
         dataset_name (str): Dataset used (used to determine save path and legend position).
         log (bool): If True, apply log10 transform to the values before averaging and plotting.
     """
-    plt.figure(figsize=(9, 6))
+    plt.figure(figsize=(9, 7))
     i = 0
 
     # Plot each algorithm's mean and std across runs
     for algo_name in legends:
+        value_to_plot = []
+        for s in values[algo_name].keys():
+            for l in values[algo_name][s]:
+                value_to_plot.append(l)
         if log:
-            avg_values = np.mean([np.log10(v) for v in values[algo_name]], axis=0)
-            avg_values_var = np.std([np.log10(v) for v in values[algo_name]], axis=0)
+            avg_values = np.mean([np.log10(v) for v in value_to_plot], axis=0)
+            avg_values_var = np.std([np.log10(v) for v in value_to_plot], axis=0)
         else:
-            avg_values = np.mean(values[algo_name], axis=0)
-            avg_values_var = np.std(values[algo_name], axis=0)
+            avg_values = np.mean(value_to_plot, axis=0)
+            avg_values_var = np.std(value_to_plot, axis=0)
 
-        plt.plot(epochs["Local"][0], avg_values, linestyle='-', color=COLORS[i], label=algo_name, linewidth=5)
-        plt.fill_between(epochs["Local"][0], avg_values - avg_values_var, avg_values + avg_values_var, alpha=0.2,
+        epochs_axis = np.linspace(0, len(avg_values)-1, len(avg_values))
+        # plt.plot(np.log10(epochs_axis), avg_values, linestyle='-', color=COLORS[i], label=algo_name, linewidth=5)
+        # plt.fill_between(np.log10(epochs_axis), avg_values - avg_values_var, avg_values + avg_values_var, alpha=0.2,
+        plt.plot(epochs_axis, avg_values, linestyle='-', color=COLORS[i], label=algo_name, linewidth=5)
+        plt.fill_between(epochs_axis, avg_values - avg_values_var, avg_values + avg_values_var, alpha=0.2,
                          color=COLORS[i])
         i += 1
 
@@ -56,21 +66,28 @@ def plot_values(epochs, values, legends, metric_name, dataset_name, log=False):
 
     # Heuristic for legend placement depending on dataset and metric
     if (metric_name == "log(Test loss)" and dataset_name == "mnist"):
-        loc = "upper right"
+        loc = "lower left"
     elif (metric_name == "Test accuracy" and dataset_name == "mnist"):
         loc = "lower right"
     else:
         loc = "lower left"
 
     if dataset_name in ["mnist", "synth"]:
-        plt.legend(fontsize=FONTSIZE, loc=loc)
+        plt.legend(fontsize=FONTSIZE, loc=loc, ncol=2)
 
     # Save figure to disk
     root = get_project_root()
     folder = f'{root}/pictures/{dataset_name}'
     create_folder_if_not_existing(folder)
-    plt.savefig(f"{folder}/{metric_name}_b{BATCH_SIZE[dataset_name]}_LR{STEP_SIZE[dataset_name]}_m{MOMENTUM[dataset_name]}.pdf",
-                bbox_inches='tight', dpi=600)
+    if dataset_name in SPLIT.keys():
+        ID = (f"{metric_name}_N{NB_CLIENTS[dataset_name]}_b{BATCH_SIZE[dataset_name]}_LR{STEP_SIZE[dataset_name]}_"
+              f"s{SCHEDULER_PARAMS[dataset_name][0]}_m{MOMENTUM[dataset_name]}_inner{inner_iterations}_"
+              f"bAl{batch_size_alignement}_{SPLIT[dataset_name]}")
+    else:
+        ID = (f"{metric_name}_N{NB_CLIENTS[dataset_name]}_b{BATCH_SIZE[dataset_name]}_LR{STEP_SIZE[dataset_name]}_"
+              f"s{SCHEDULER_PARAMS[dataset_name][0]}_m{MOMENTUM[dataset_name]}_inner{inner_iterations}_"
+              f"bAl{batch_size_alignement}_")
+    plt.savefig(f"{folder}/{ID}.pdf", bbox_inches='tight', dpi=600)
 
     # Print final metric value (e.g., accuracy or log-loss) in LaTeX tabular format for paper inclusion
     print("\\begin{tabular}{|c|c|}")
@@ -78,16 +95,21 @@ def plot_values(epochs, values, legends, metric_name, dataset_name, log=False):
     print(f"Algorithm & {metric_name} \\\\")
     print("\\hline")
     for algo_name in legends:
+        value_to_plot = []
+        for s in values[algo_name].keys():
+            for l in values[algo_name][s]:
+                value_to_plot.append(l)
         if log:
-            final_value = np.mean([np.log10(v) for v in values[algo_name]], axis=0)[-1]
+            final_value = np.mean([np.log10(v) for v in value_to_plot], axis=0)[-1]
         else:
-            final_value = np.mean(values[algo_name], axis=0)[-1]
+            final_value = np.mean(value_to_plot, axis=0)[-1]
         print(f"{algo_name} & {final_value:.4f} \\\\")
     print("\\hline")
     print("\\end{tabular}")
 
 
-def plot_weights(weights, dataset_name, algo_name, name="weights", x_axis=None):
+def plot_weights(weights, dataset_name, algo_name, inner_iterations: int, batch_size_alignement: int,
+                 name="weights", x_axis=None):
     """
     Plot the evolution of per-client weights over training rounds.
 
@@ -131,5 +153,12 @@ def plot_weights(weights, dataset_name, algo_name, name="weights", x_axis=None):
     root = get_project_root()
     folder = f'{root}/pictures/{dataset_name}'
     create_folder_if_not_existing(folder)
-    plt.savefig(f"{folder}/{algo_name}_{name}_b{BATCH_SIZE[dataset_name]}_LR{STEP_SIZE[dataset_name]}_m{MOMENTUM[dataset_name]}.pdf",
-                bbox_inches='tight', dpi=600)
+    if dataset_name in SPLIT.keys():
+        ID = (f"N{NB_CLIENTS[dataset_name]}_b{BATCH_SIZE[dataset_name]}_LR{STEP_SIZE[dataset_name]}_"
+              f"s{SCHEDULER_PARAMS[dataset_name][0]}_m{MOMENTUM[dataset_name]}_inner{inner_iterations}_"
+              f"bAl{batch_size_alignement}_{SPLIT[dataset_name]}")
+    else:
+        ID = (f"N{NB_CLIENTS[dataset_name]}_b{BATCH_SIZE[dataset_name]}_LR{STEP_SIZE[dataset_name]}_"
+              f"s{SCHEDULER_PARAMS[dataset_name][0]}_m{MOMENTUM[dataset_name]}_inner{inner_iterations}_"
+              f"bAl{batch_size_alignement}")
+    plt.savefig(f"{folder}/{algo_name}_{name}_{ID}.pdf", bbox_inches='tight', dpi=600)